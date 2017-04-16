a0df3ab36da88983c6d80be6454dac3ecd5b8260
testlab01 <mytestinglab1@gmail.com>
2017-04-16 20:18:54 +0530

    open a new socket for listen

--- socketPrograming/server.py
+++ socketPrograming/server.py
@@ -1,1 +1,21 @@
+import socket
 
+class ServerSocket:
+
+	def __init__(self):
+		self.serverSocket = 'none';
+
+	def open_socket(self, host = 'localhost', port = 8080):
+		self.host = host
+		self.port = port
+		if self.serverSocket == 'none':
+			try:
+				self.serverSocket = socket.socket()
+			except socket.error as errorMsg:
+				print(errorMsg)
+		else:
+			self.serverSocket
+
+
+socketS = ServerSocket()
+socketS.open_socket()
